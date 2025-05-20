00fa445b62f57a05100c2c30b1226454007a26c7
jerpism <jere.m@live.fi>
2025-05-20 14:47:03 +0300

    Working character cell print

--- drivers/vga.s
+++ drivers/vga.s
@@ -14,10 +14,10 @@ SECTION .text
 
 ; print_cell(u8 character, u8 color, i32 offset)
 print_cell:
-    mov     eax, VGA_MEM            ; eax points to screen memory
-    mov     cx, WORD [esp]      ; get character and color
-    mov     edx, DWORD [esp - 4]    ; get offset
-    mov     WORD [eax + edx], cx         ; write out character
+    mov     eax, VGA_MEM          ; eax points to screen memory
+    mov     ecx, DWORD [esp+4]    ; get character and color
+    mov     edx, DWORD [esp+8]    ; get offset
+    mov     WORD [eax + edx], cx  ; write out character
 
     ret
 
--- kernel/kernel.s
+++ kernel/kernel.s
@@ -5,12 +5,11 @@ extern print_cell
 
 main:
     mov eax, 0
-    mov dx, 0x4441
+    mov dx, 0x4f42
 
     push eax
     push edx
 
     call print_cell
 
-jmp $
    jmp $

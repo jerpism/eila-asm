92d74f13c8421d8bd6ceea17151bcfbc192bd8b8
jerpism <jere.m@live.fi>
2025-05-21 13:36:26 +0300

    new main

--- kernel/kernel.s
+++ kernel/kernel.s
@@ -3,6 +3,7 @@ global main
 
 extern print_character
 extern print_str
+extern test
 
 main:
     mov eax, 0
@@ -11,10 +12,13 @@ main:
 
     call print_str
 
+
     mov eax, -1
     mov edx, -1
     mov ecx, STR_TEST2
     call print_str
+
+    call test
     jmp $
 
 

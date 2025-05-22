26df811f0c590969b467072ed0a3a7f79f4d4057
jerpism <jere.m@live.fi>
2025-05-22 14:38:20 +0300

    new main

--- kernel/kernel.s
+++ kernel/kernel.s
@@ -1,28 +1,23 @@
 [bits 32]
 global main
 
-extern print_char
 extern print_str
+extern pic_remap
 
 main:
-    mov eax, 0
-    mov edx, 5
-    mov ecx, STR_TEST
-
-    call print_str
-
+    ; test print
     mov eax, -1
     mov edx, -1
-    mov ecx, STR_TEST2
-
+    mov ecx, STR_TEST
     call print_str
 
-    mov eax, 'A'
-    call print_char
+    ; remap pic, puts IMR value in ax
+    mov ecx, 0x20
+    mov edx, 0x28
+    call pic_remap
 
     jmp $
 
 
-STR_TEST: db `Hhello\nhi`, 0x0
-STR_TEST2: db 'Hihi', 0x0
+STR_TEST: db `Booted into protected mode succesfully`, 0x0
 
